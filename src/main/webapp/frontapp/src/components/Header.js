2f4ed29958f4bb255ce0f9e3aaf9f106df56784e
egdg4587 <egdg7777@gmail.com>
2024-07-05 11:06:39 +0900

    ?

---
+++ src/main/webapp/frontapp/src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+function Header(props) {
+    return (
+
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Link to="/" className="navbar-brand">
+          블로그홈
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/saveForm" className="nav-link">
+              글쓰기
+            </Link>
+            <Link to="/" className="nav-link">
+              로그아웃
+            </Link>
+            <Link to="/loginForm" className="nav-link">
+              로그인
+            </Link>
+            <Link to="/joinForm" className="nav-link">
+              회원가입
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+}
+
+export default Header;
